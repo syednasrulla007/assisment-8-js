0ffb4555cd5fb5352bc2c3bb968139f72fe675ed
syednasrulla007 <syednasrulla085@gmail.com>
2022-12-30 19:01:36 +0530

    done

---
+++ index.js
@@ -0,0 +1,96 @@
+let studentRecords = [ {name: 'John', id: 123, marks : 98 },
+          {name: 'Baba', id: 101, marks : 23 },
+          {name: 'yaga', id: 200, marks : 45 },
+          {name: 'Wick', id: 115, marks : 75 } ] 
+
+        // 1)  let res=[]
+        //   for(i=0;i<studentRecords.length;i++){
+        //     obj=studentRecords[i];
+        //     res.push(obj.name.toUpperCase())
+        //   }
+        //   console.log(res)
+
+
+        // 2)  let res=[]
+        //   for(i=0;i<studentRecords.length;i++){
+        //     obj=studentRecords[i];
+        //     if(obj.marks>50)
+        //     res.push(obj)
+        //   }
+        //   console.log(res)
+
+
+          // let res=[]
+          // for(i=0;i<studentRecords.length;i++){
+          //   obj=studentRecords[i];
+          //   if(obj.marks>50 && obj.id>120)
+          //   res.push(obj)
+          // }
+          // console.log(res)
+
+
+          // let res=[]
+          // let sum=0
+          // for(i=0;i<studentRecords.length;i++){
+          //   obj=studentRecords[i];
+          //   if(obj.marks>50)
+          //   res.push(obj.marks)
+          // }
+          // for(let j=0;j<res.length;j++){
+            
+          //   sum=sum+res[j]
+          // }
+          // console.log(sum)
+
+
+        
+          // let res=[]
+          // for(i=0;i<studentRecords.length;i++){
+          //   obj=studentRecords[i];
+          //   if(obj.marks>50)
+          //   res.push(obj.name)
+          // }
+          // console.log(res)
+
+
+
+          // let res=[]
+          // let sum=0
+          // for(i=0;i<studentRecords.length;i++){
+          //   obj=studentRecords[i];
+          //   if(obj.id>120)
+          //   res.push(obj.marks)
+          // }
+          // for(let j=0;j<res.length;j++){
+            
+          //   sum=sum+res[j]
+          // }
+          // console.log(sum)
+
+
+
+          // let res=[]
+          // let sum=0
+          // for(i=0;i<studentRecords.length;i++){
+          //   obj=studentRecords[i];
+          //   if(obj.id+15>50)
+          //   res.push(obj.marks)
+          // }
+          // for(let j=0;j<res.length;j++){
+            
+          //   sum=sum+res[j]
+          // }
+          // console.log(sum)
+
+
+
+
+          // let obj1={name:"Pranit",class:"1",rollno:21}
+          // let obj2={name:"Ritesh",class:"1",rollno:22}
+          // let obj3={name:"Anuj",class:"1",rollno:23}
+          // let obj4={name:"Rudra",class:"1",rollno:24}
+          // let obj5={name:"Prajakta",class:"1",rollno:25}
+          // let obj6={name:"Ajay",class:"1",rollno:26}
+
+          // let arr=[obj1,obj2,obj3,obj4,obj5,obj6]
+          // console.log(arr)
